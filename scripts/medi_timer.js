4c09a95be4244eab6fd0e53190b0f6f8b005564a
TheHuntsman4 <anikethvij464@gmail.com>
2023-08-09 02:05:12 +0530

    Fix Fixes a few errors in meditation pages and makes them responsive

--- scripts/medi_timer.js
+++ scripts/medi_timer.js
@@ -3,81 +3,85 @@ const play = document.querySelector(".play");
 const replay = document.querySelector(".replay");
 const outline = document.querySelector(".moving-outline circle");
 const video = document.querySelector(".vid-container video");
-//Sounds
+// Sounds
 const sounds = document.querySelectorAll(".sound-picker button");
-//Time Display
+// Time Display
 const timeDisplay = document.querySelector(".time-display");
 const outlineLength = outline.getTotalLength();
-//Duration
+// Duration
 const timeSelect = document.querySelectorAll(".time-select button");
-let fakeDuration = 600;
+let fakeDuration = 600; // Default duration in seconds
 
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
 timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
   fakeDuration % 60
-)}`;
+).toString().padStart(2, '0')}`;
 
-sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
+sounds.forEach((sound) => {
+  sound.addEventListener("click", function () {
     song.src = this.getAttribute("data-sound");
     video.src = this.getAttribute("data-video");
     checkPlaying(song);
   });
 });
 
-play.addEventListener("click", function() {
+play.addEventListener("click", function () {
   checkPlaying(song);
 });
 
-replay.addEventListener("click", function() {
-    restartSong(song);
-    
-  });
+replay.addEventListener("click", function () {
+  restartSong(song);
+});
 
-
-const restartSong = song =>{
-    let currentTime = song.currentTime;
-    song.currentTime = 0;
-    console.log("ciao")
-
-}
-
-timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
-    fakeDuration = this.getAttribute("data-time");
+timeSelect.forEach((option) => {
+  option.addEventListener("click", function () {
+    fakeDuration = parseFloat(this.getAttribute("data-time"));
     timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
       fakeDuration % 60
-    )}`;
+    ).toString().padStart(2, '0')}`;
+
+    if (fakeDuration === 120) { // 2 minutes in seconds
+      song.currentTime = 0; // Reset song to the beginning
+      song.pause(); // Pause the song
+      video.pause(); // Pause the video
+      play.src = "../assets/svg/play.svg"; // Change play button icon
+    }
   });
 });
 
-const checkPlaying = song => {
+const checkPlaying = (song) => {
   if (song.paused) {
     song.play();
     video.play();
-    play.src = "./svg/pause.svg";
-
+    play.src = "../assets/svg/pause.svg";
   } else {
     song.pause();
     video.pause();
-    play.src = "./svg/play.svg";
+    play.src = "../assets/svg/play.svg";
   }
 };
 
-song.ontimeupdate = function() {
+const restartSong = (song) => {
+  song.currentTime = 0;
+  play.src = "../assets/svg/pause.svg";
+  song.play();
+  video.play();
+};
+
+song.ontimeupdate = function () {
   let currentTime = song.currentTime;
   let elapsed = fakeDuration - currentTime;
   let seconds = Math.floor(elapsed % 60);
   let minutes = Math.floor(elapsed / 60);
-  timeDisplay.textContent = `${minutes}:${seconds}`;
+  timeDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
   let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
   outline.style.strokeDashoffset = progress;
 
   if (currentTime >= fakeDuration) {
     song.pause();
     song.currentTime = 0;
-    play.src = "./svg/play.svg";
+    play.src = "../assets/svg/play.svg";
     video.pause();
   }
 };
