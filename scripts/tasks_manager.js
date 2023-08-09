8aa497db23540d78fb2b9a9edcbd2a970e3a3b9c
TheHuntsman4 <anikethvij464@gmail.com>
2023-08-09 09:28:52 +0530

    Feat Updates task_result.html

--- scripts/tasks_manager.js
+++ scripts/tasks_manager.js
@@ -5,6 +5,9 @@ const taskButtons = document.querySelectorAll('.task-button');
 taskButtons.forEach(button => {
   button.addEventListener('click', () => {
     const taskText = button.querySelector('h2').textContent;
+    const lottieSrc = button.getAttribute('data-lottie-src');
+    console.log(lottieSrc);
+    localStorage.setItem('lottieSrc',lottieSrc)
     localStorage.setItem('selectedTask', taskText);
     window.location.href = '../Pages/task_rewards.html'; // Redirect to the rewards page
   });
--- scripts/tasks_rewards.js
+++ scripts/tasks_rewards.js
@@ -5,6 +5,8 @@ const rewardButtons = document.querySelectorAll('.task-button');
 rewardButtons.forEach(button => {
   button.addEventListener('click', () => {
     const rewardText = button.querySelector('h2').textContent;
+    const rewardlottieSrc = button.querySelector('lottie-player').getAttribute('src');
+    localStorage.setItem('rewardlottieSrc',rewardlottieSrc)
     localStorage.setItem('selectedReward', rewardText);
     window.location.href = '../Pages/task_result.html'; // Redirect to the result page
   });
